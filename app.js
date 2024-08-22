dd43d19b57fc47c5c2344cc6871b25b96d5c156b
MengChi Chang <mengdesigntw@gmail.com>
2024-08-22 12:15:40 +0800

    support double digit chaining with equal-key

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ const equalKey = document.querySelector('#equal');
 
 const key7 = document.querySelector('#key7');
 
-//create variable to store input value and operator
+//create variable to store final input value, operator, temporary value(e.g 77)
+let temporary = '';
 let inputs = [];
 let operator = '';
 currentValue.textContent = inputs[0] || 0;
@@ -27,28 +28,45 @@ calHistory.textContent = '';
 //create click event to the digit pad and show it on the display
 key7.addEventListener('click', function (e) {
   const value = +e.target.textContent; // number 7
-  currentValue.textContent = value;
-  //show history if inputs[0] has value
-  if (inputs[0] || inputs[0] == 0) {
+  //show history if inputs[0] has value, and toggle the focused state off when temporary is falsy
+  if ((inputs[0] || inputs[0] === 0) && !temporary) {
     calHistory.textContent = `${inputs[0]} ${operator}`;
     plusKey.classList.toggle('focused');
   }
-  inputs.push(value);
-  console.log(inputs);
+  //store in temp
+  temporary += value; //temp: '7777' string
+  currentValue.textContent = temporary;
+  console.log(inputs, temporary);
 });
 
 //create event to the operator
 plusKey.addEventListener('click', function (e) {
-  const value = e.target.textContent;
-  operator = value; // string '+'
-  if (!inputs[0]) inputs.push(0);
+  operator = e.target.textContent; // string '+'
+  if (!inputs[0]) {
+    //tap 777 +
+    if (temporary) {
+      inputs.push(+temporary);
+    } else {
+      //tap +
+      temporary = '0';
+      inputs.push(+temporary);
+    }
+  }  
+  temporary = '';
+  
+  //with input[0] has value do nothing
+
   plusKey.classList.toggle('focused');
-  console.log(inputs, operator)
+  console.log(inputs, operator, temporary);
 });
 
 //create event to end the cal-cycle
 equalKey.addEventListener('click', function (e) {
   if (!operator) return;
+  if (operator && !temporary) return plusKey.classList.toggle('focused');
+  inputs.push(+temporary);
+  temporary = '';
+  console.log(inputs, temporary);
   if ((operator = '+')) {
     const sum = inputs.reduce((total, value) => {
       return total + value;
@@ -57,5 +75,5 @@ equalKey.addEventListener('click', function (e) {
     inputs = [sum];
   }
   calHistory.textContent = '';
-  console.log(inputs)
+  console.log(inputs, temporary);
 });
