b515ac527cf6805cff97fb705b4eda9bbf5c70e1
MengChi Chang <mengdesigntw@gmail.com>
2024-08-22 10:29:40 +0800

    create function for plus and equal

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log('hi')
+console.log('hi');
+
+//create variables for nodes
+const currentValue = document.querySelector('.current');
+const calHistory = document.querySelector('.history');
+const clearKey = document.querySelector('#clear');
+const positiveMinusKey = document.querySelector('#positive-minus');
+const getPercentageKey = document.querySelector('#percentage');
+const divideKey = document.querySelector('#divide');
+const multiplyKey = document.querySelector('#multiply');
+const minusKey = document.querySelector('#minus');
+const plusKey = document.querySelector('#plus');
+const decimalKey = document.querySelector('#decimal');
+const deleteKey = document.querySelector('#delete');
+const equalKey = document.querySelector('#equal');
+
+const key7 = document.querySelector('#key7');
+
+//create variable to store input value and operator
+let inputs = [];
+let operator = '';
+currentValue.textContent = inputs[0] || 0;
+calHistory.textContent = '';
+
+//create click event to the digit pad and show it on the display
+key7.addEventListener('click', function (e) {
+  const value = +e.target.textContent; // number 7
+  currentValue.textContent = value;
+  //show history if inputs[0] has value
+  if (inputs[0]) {
+    calHistory.textContent = `${inputs[0]} ${operator}`;
+  }
+  inputs.push(value);
+  console.log(inputs);
+});
+
+//create event to the operator
+plusKey.addEventListener('click', function (e) {
+  const value = e.target.textContent;
+  operator = value; // string '+'
+});
+
+//create event to end the cal-cycle
+equalKey.addEventListener('click', function (e) {
+  if (!operator) return;
+  if ((operator = '+')) {
+    const sum = inputs.reduce((total, value) => {
+      return total + value;
+    }, 0);
+    currentValue.textContent = sum;
+  }
+  calHistory.textContent = '';
+  inputs = [];
+});
