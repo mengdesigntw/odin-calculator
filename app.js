a65c8f3fb657acf708200c8ad6199ee0426907af
MengChi Chang <mengdesigntw@gmail.com>
2024-08-23 12:53:40 +0800

    refactor code on equal key

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const equalKey = document.querySelector('#equal');
 
 const key7 = document.querySelector('#key7');
 
-//create variable to store final input value, operator, temporary value(e.g 77)
+//state variables
 let temporary = '';
 let inputs = [];
 let operator = '';
@@ -33,79 +33,56 @@ function plus(arr) {
   return sum;
 }
 
-
+///////DIGIT//////
 key7.addEventListener('click', function (e) {
-//store the value in temporary as string and concat
-//then show it on currentValue
-temporary += +e.target.textContent
-currentValue.textContent = temporary
-
-//if there's stored operator, then show inputs[0] and stored operator on the history. 
-//also toggle the operator focus off only when first entry
-if(operator) {
-    calHistory.textContent = `${inputs[0]} ${operator}`;
-    if(temporary.length == 1) plusKey.classList.toggle('focused')
-} else {
-    inputs = []
-}
-console.log(inputs, operator, temporary);
-  
+  temporary += e.target.textContent; //string
+  currentValue.textContent = temporary;
+  if (operator) {
+    calHistory.textContent = `${inputs[inputs.length - 1]} ${operator}`;
+    if (temporary.length == 1) plusKey.classList.toggle('focused');   //only when first entry
+  } else {
+    inputs = []; //start clean
+  }
 });
 
-
+///OPERATOR////
 plusKey.addEventListener('click', function (e) {
-  //when there is temporary then push to the inputs, then clear temporary.
-  //and toggole the focus on.
-
-  //if there's stored operator, display cal history with previous input and operator
-  //then do the math and re-assign the value to input and display in currentValue
-
-  //after above done, re-assign '+' to operator
   if (temporary) {
     inputs.push(+temporary);
-    temporary = '';
     plusKey.classList.toggle('focused');
+    if (inputs.length > 2) inputs.shift(); //remove the first one
     if (operator == '+') {
-      calHistory.textContent = `${inputs[0]} ${operator}`;
       const sum = plus(inputs);
-      inputs = [sum];
-      currentValue.textContent = inputs[0];
+      inputs.push(sum);
+      inputs.shift();
+      calHistory.textContent = `${inputs[0]} ${operator}`;
+      currentValue.textContent = inputs[1];
     }
-    operator = e.target.textContent;
   }
-  //when there is no temporary then do nothing
-  // unless the intitial [] is also empty, then push a 0 as the first input
-  //and toggle on focus on
-  //assign '+' to operator
-  if (!temporary && !inputs.length) {
-    inputs.push(0);
-    plusKey.classList.toggle('focused');
-    operator = e.target.textContent;
+  if (!temporary) {
+    if (!inputs.length) {
+      inputs.push(0); //initiate a 0
+      plusKey.classList.toggle('focused');
+    } else if (!operator) { // 7 = +
+      plusKey.classList.toggle('focused');
+    }
   }
-  console.log(inputs, operator, temporary);
+  temporary = '';
+  operator = e.target.textContent;
 });
 
-//create event to end the cal-cycle
 equalKey.addEventListener('click', function (e) {
-  if (!operator) return;
-  if (operator && !temporary) {
-    plusKey.classList.toggle('focused');
-    operator = '';
-    calHistory.textContent = '';
-    return;
+  if (operator && !temporary) plusKey.classList.toggle('focused');
+  if (temporary) {
+    inputs.push(+temporary); // '7'
+    temporary = '';
+    if (inputs.length > 2) inputs.shift();
   }
-  //if there is temporary
-  inputs.push(+temporary);
-  temporary = '';
-  console.log(inputs, temporary);
   if (operator == '+') {
-    const sum = inputs.reduce((total, value) => {
-      return total + value;
-    }, 0);
-    currentValue.textContent = sum;
+    const sum = plus(inputs);
     inputs = [sum];
+    currentValue.textContent = sum;
   }
   operator = '';
   calHistory.textContent = '';
-  console.log(inputs, operator, temporary);
 });
