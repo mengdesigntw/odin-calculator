e38a6db26716af082af769a4157cabc447818578
MengChi Chang <mengdesigntw@gmail.com>
2024-09-06 12:12:41 +0800

    refining logic v1

--- modules/eventHandlers.js
+++ modules/eventHandlers.js
@@ -31,7 +31,6 @@ function updateDisplay() {
     setCalHistory(displayInputs[0], storedOperator, displayInputs[1]);
     setCurrentDisplay(displayTotal);
     if (total == 'NaN' || total == 'Infinity') {
-      // setClearDisplay();
       setCalHistory();
       setCurrentDisplay('Naughty ❤️');
     }
@@ -75,19 +74,17 @@ function handleDigits(e) {
     storedOperator = ''; // H7-1 H7m-1
   } else if (inputs.length && temporary) {
     if (tempOperator) {
+      temporary = e.target.textContent;
       if (!total) {
-        temporary = e.target.textContent;
         storedOperator = tempOperator;
         tempOperator = ''; // G7-1 H1
       } else if (total) {
         if (total !== 'NaN' && total !== 'Infinity') {
-          temporary = e.target.textContent;
           storedOperator = tempOperator;
           tempOperator = '';
           inputs = [+total];
           total = ''; //H1-7-1
         } else if (total == 'NaN' || total == 'Infinity') {
-          temporary = e.target.textContent;
           inputs = [];
           tempOperator = '';
           total = ''; //H1-7m-1
@@ -107,14 +104,13 @@ function handleModifier() {
   } else if (!inputs.length && !temporary) {
     temporary = '-0'; //D2
   } else if (inputs.length && !temporary) {
+    inputs = [];
     if (total !== 'NaN' && total !== 'Infinity') {
-      inputs = [];
       temporary = total;
       temporary = modOpposite(temporary);
       total = '';
       storedOperator = ''; //H7-2
     } else if (total !== 'NaN' || total !== 'Infinity') {
-      inputs = [];
       total = '';
       storedOperator = ''; //H7m-2
     }
@@ -147,21 +143,19 @@ function handleModifier() {
 
 function handlePercentage() {
   const lastInput = inputs[inputs.length - 1];
-  if (!inputs.length && temporary) {
-    temporary = `${modMathResult(+temporary / 100)}`; //C G3
-  } else if (!inputs.length && !temporary) {
+  if (!inputs.length && !temporary) {
     //D3
+  } else if (!inputs.length && temporary) {
+    temporary = `${modMathResult(+temporary / 100)}`; //C G3
   } else if (inputs.length && !temporary) {
+    inputs = [];
+    storedOperator = '';
     if (total !== 'NaN' && total !== 'Infinity') {
-      inputs = [];
       temporary = total;
       temporary = `${modMathResult(+total / 100)}`;
-      total = '';
-      storedOperator = ''; //H7-3
+      total = ''; //H7-3
     } else if (total !== 'NaN' || total !== 'Infinity') {
-      inputs = [];
-      total = '';
-      storedOperator = ''; //H7m-3
+      total = ''; //H7m-3
     }
   } else if (inputs.length && temporary) {
     if (tempOperator) {
@@ -191,10 +185,10 @@ function handlePercentage() {
 }
 
 function handleClear() {
-  if (!inputs.length && temporary) {
-    temporary = ''; //D G4
-  } else if (!inputs.length && !temporary) {
+  if (!inputs.length && !temporary) {
     //D0
+  } else if (!inputs.length && temporary) {
+    temporary = ''; //D G4
   } else if (inputs.length && !temporary) {
     inputs = [];
     storedOperator = '';
@@ -207,15 +201,13 @@ function handleClear() {
         inputs = []; //G7-4 H4
       }
       if (total) {
+        total = '';
+        inputs = [];
         if (total !== 'NaN' && total !== 'Infinity') {
           toggleOperatorFocus(tempOperator);
-          tempOperator = '';
-          total = '';
-          inputs = []; //H1-7-4
+          tempOperator = ''; //H1-7-4
         } else if (total == 'NaN' || total == 'Infinity') {
-          inputs = [];
           tempOperator = '';
-          total = '';
           temporary = ''; //H1-7m-4
         }
       }
@@ -234,12 +226,9 @@ function handleClear() {
 }
 
 function handleDecimal(e) {
+  if (temporary.includes('.')) return; //C4 H3-4
   if (!inputs.length && temporary) {
-    if (!temporary.includes('.')) {
-      temporary += e.target.textContent; // A E G5
-    } else if (temporary.includes('.')) {
-      return; //C4
-    }
+    if (!temporary.includes('.')) temporary += e.target.textContent; // A E G5
   } else if (!inputs.length && !temporary) {
     temporary = '0.'; //D4
   } else if (inputs.length && !temporary) {
@@ -249,30 +238,27 @@ function handleDecimal(e) {
     total = ''; //H7-5 //H7m-5
   } else if (inputs.length && temporary) {
     if (tempOperator) {
+      temporary = '0.';
       if (!total) {
-        temporary = '0.';
         storedOperator = tempOperator;
         tempOperator = ''; //G7-5 H5
       } else if (total) {
         if (total !== 'NaN' && total !== 'Infinity') {
-          temporary = '0.';
           storedOperator = tempOperator;
           tempOperator = '';
           inputs = [+total];
           total = ''; //H1-7-5
         } else if (total == 'NaN' || total == 'Infinity') {
-          temporary = '0.';
           inputs = [];
           tempOperator = '';
           total = ''; //H1-7m-5
         }
       }
     } else if (!tempOperator) {
-      if (!temporary.includes('.')) {
-        temporary += e.target.textContent; //H1-4
-      } else if (temporary.includes('.')) {
-        return; //H3-4
-      }
+      if (!temporary.includes('.')) temporary += e.target.textContent; //H1-4
+      // else if (temporary.includes('.')) {
+      //   return; //H3-4
+      // }
     }
   }
   updateDisplay();
@@ -285,9 +271,9 @@ function handleDelete() {
     } else if ((temporary.length > 1 && !temporary.startsWith('-')) || (temporary.length > 2 && temporary.startsWith('-'))) {
       temporary = temporary.slice(0, -1); //A5,A1-5
     }
-  } else if (!inputs.length && !temporary) {
-    updateDisplay();
-  } else if (inputs.length && !temporary) {
+  }
+  // else if (!inputs.length && !temporary) {}//
+  else if (inputs.length && !temporary) {
     inputs = [];
     storedOperator = '';
     total = ''; //H7-6 H7m-6
@@ -329,22 +315,16 @@ function handleDelete() {
 }
 
 function handleEqual() {
-  if (!inputs.length && temporary) {
-    return;
-  } else if (!inputs.length && !temporary) {
-    return; //D6
-  } else if (inputs.length && !temporary) {
+  if (!inputs.length) return; //D6
+  if (total == 'NaN' || total == 'Infinity') return; //H7m-0 H1-7m-7
+  if (inputs.length && !temporary) {
     if (total !== 'NaN' && total !== 'Infinity') {
       inputs[0] = +total;
-      total = `${modMathResult(doTheMath(storedOperator, inputs[0], inputs[1]))}`;
-      updateDisplay(); //H7-0
-    } else if (total == 'NaN' || total == 'Infinity') {
-      return; //H7m-0
-      // storedOperator = '';
-      // total = '';
-      // inputs = [];
-      // updateDisplay(); 
+      total = `${modMathResult(doTheMath(storedOperator, inputs[0], inputs[1]))}`; //H7-0
     }
+    // else if (total == 'NaN' || total == 'Infinity') {
+    //   return; //H7m-0
+    // }
   } else if (inputs.length && temporary) {
     if (tempOperator) {
       if (!total) {
@@ -355,11 +335,6 @@ function handleEqual() {
         tempOperator = '';
         total = `${modMathResult(doTheMath(storedOperator, inputs[0], inputs[1]))}`;
         updateDisplay(); //G7-7 H7
-        // if (total == 'NaN' || total == 'Infinity') {
-        //   storedOperator = '';
-        //   total = '';
-        //   inputs = [];
-        // }
       } else if (total) {
         if (total !== 'NaN' && total !== 'Infinity') {
           inputs = [+total, +temporary];
@@ -369,23 +344,19 @@ function handleEqual() {
           tempOperator = '';
           total = `${modMathResult(doTheMath(storedOperator, inputs[0], inputs[1]))}`;
           updateDisplay(); //H1-7-7
-        } else if (total == 'NaN' || total == 'Infinity') {
-          return; //H1-7m-7
         }
+        // else if (total == 'NaN' || total == 'Infinity') {
+        //   return; //H1-7m-7
+        // }
       }
     } else if (!tempOperator) {
       inputs.push(+temporary);
       temporary = '';
       toggleOperatorFocus(storedOperator);
-      total = `${modMathResult(doTheMath(storedOperator, inputs[0], inputs[1]))}`;
-      updateDisplay(); //H1-6
-      // if (total == 'NaN' || total == 'Infinity') {
-      //   total = '';
-      //   inputs = [];
-      //   storedOperator = '';
-      // }
+      total = `${modMathResult(doTheMath(storedOperator, inputs[0], inputs[1]))}`; //H1-6
     }
   }
+  updateDisplay();
 }
 
 function handleOperator(e) {
@@ -393,51 +364,40 @@ function handleOperator(e) {
     inputs.push(+temporary); //H G7
     tempOperator = e.target.textContent;
     toggleOperatorFocus(tempOperator);
-    updateDisplay();
   } else if (!inputs.length && !temporary) {
     inputs.push(0);
     tempOperator = e.target.textContent;
     toggleOperatorFocus(tempOperator);
-    temporary = `${inputs[inputs.length - 1]}`;
-    updateDisplay(); //DF7
+    temporary = `${inputs[inputs.length - 1]}`; //DF7
   } else if (inputs.length && !temporary) {
+    tempOperator = e.target.textContent;
+    toggleOperatorFocus(tempOperator);
+    storedOperator = '';
+    total = '';
     if (total !== 'NaN' && total !== 'Infinity') {
       inputs = [+total];
-      temporary = `${inputs[inputs.length - 1]}`;
-      tempOperator = e.target.textContent;
-      toggleOperatorFocus(tempOperator);
-      storedOperator = '';
-      total = '';
-      updateDisplay(); //H7-7
+      temporary = `${inputs[inputs.length - 1]}`; //H7-7
     } else if (total == 'NaN' || total == 'Infinity') {
       inputs = [0];
-      temporary = `${inputs[inputs.length - 1]}`;
-      tempOperator = e.target.textContent;
-      toggleOperatorFocus(tempOperator);
-      storedOperator = '';
-      total = '';
-      updateDisplay(); //H7m-7
+      temporary = `${inputs[inputs.length - 1]}`; //H7m-7
     }
   } else if (inputs.length && temporary) {
     if (tempOperator) {
       if (!total) {
         toggleOperatorFocus(tempOperator);
         tempOperator = e.target.textContent;
-        toggleOperatorFocus(tempOperator);
-        updateDisplay(); //G7-0 H0
+        toggleOperatorFocus(tempOperator); //G7-0 H0
       } else if (total) {
         if (total !== 'NaN' && total !== 'Infinity') {
           toggleOperatorFocus(tempOperator);
           tempOperator = e.target.textContent;
-          toggleOperatorFocus(tempOperator);
-          updateDisplay(); //H1-7-0
+          toggleOperatorFocus(tempOperator); //H1-7-0
         } else if (total == 'NaN' || total == 'Infinity') {
           inputs = [0];
           temporary = `${inputs[inputs.length - 1]}`;
           tempOperator = e.target.textContent;
           toggleOperatorFocus(tempOperator);
-          total = '';
-          updateDisplay(); //H1-7m-0
+          total = ''; //H1-7m-0
         }
       }
     } else if (!tempOperator) {
@@ -447,17 +407,11 @@ function handleOperator(e) {
       toggleOperatorFocus(tempOperator);
       total = `${modMathResult(doTheMath(storedOperator, inputs[0], inputs[1]))}`;
       temporary = total;
-      storedOperator = '';
-      updateDisplay(); //H1-7
-      if (total == 'NaN' || total == 'Infinity') {
-        // temporary = '';
-        // total = '';
-        toggleOperatorFocus(tempOperator); //H1-7m
-        // tempOperator = '';
-        // inputs = [];
-      }
+      storedOperator = ''; //H1-7
+      if (total == 'NaN' || total == 'Infinity') toggleOperatorFocus(tempOperator); //H1-7m
     }
   }
+  updateDisplay();
 }
 
 export { updateDisplay, handleDigits, handleOperator, handleDelete, handleEqual, handleDecimal, handleModifier, handlePercentage, handleClear };
